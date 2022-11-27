aebfad7f09d96353ac72409377ef1903c18cf372
nlom0218 <nlom0218@naver.com>
2022-11-27 19:43:52 +0900

    feat: 기본 Route 설정하기

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<div>홈</div>} />
+      <Route path="/rules" element={<div>규칙</div>} />
+      <Route path="/setup" element={<div>설정</div>} />
+      <Route path="/play" element={<div>게임 진행하기</div>} />
+      <Route path="/result" element={<div>게임 결과</div>} />
+    </Routes>
+  );
 }
 
 export default App;
